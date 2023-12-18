ca2472eadf2d13c1414d5afc5708ed8e5154d9d2
Louay <116605541+LouayCoding@users.noreply.github.com>
2023-12-18 10:31:32 +0100

    test

--- index.js
+++ index.js
@@ -13,34 +13,23 @@ client.on('ready', () => {
 });
 
 client.on('messageCreate', async (message) => {
-    
     const messageContent = message.content.toLowerCase();
+    console.log(process.env.GPT_COMMAND)
 
-    switch (true) {
-        case messageContent.startsWith(process.env.GPT_COMMAND):
-            if(message.author.id === '1034594784024727572') return message.reply('woeshoems mogen dit commando niet gebruiken')
-            try {
-                const answerGPT = await GPT(messageContent);
-                message.reply(answerGPT);
-            } catch (error) {
-                console.error('GPT error:', error);
-                message.reply('Oops! Something went wrong. Please try again later.');
-            }
-            break;
+    if (messageContent.startsWith(process.env.GPT_COMMAND)) {
+        console.log('ss')
+        if (message.author.id === '1034594784024727572') return message.reply('woeshoems mogen dit commando niet gebruiken')
+        try {
+            const answerGPT = await GPT(messageContent);
+            message.reply(answerGPT);
+        } catch (error) {
+            console.error('GPT error:', error);
+            message.reply('Oops! Something went wrong. Please try again later.');
+        }
 
-        case messageContent.startsWith(process.env.BING_COMMAND):
-            try {
-                const answerBING = await BING(process.env.BING_COMMAND);
-                message.reply(answerBING);
-            } catch (error) {
-                console.error('BING error:', error);
-                message.reply('Something went wrong. Please try again later.');
-            }
-            break;
-
-        default:
-            break;
     }
+
+
 });
 
 client.login(process.env.TOKEN);
